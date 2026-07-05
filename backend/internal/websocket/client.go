c69a09572548c172c140466eb5c79e7f0af1e287
its-ryann <kikayiryankim@gmail.com>
2026-07-05 11:33:06 +0300

    feat: enhance websocket Client with handleIncoming and improved WritePump logic

--- backend/internal/websocket/client.go
+++ backend/internal/websocket/client.go
@@ -51,3 +51,49 @@ func (c *Client) ReadPump() {
 		c.Hub.Broadcast <- message
 	}
 }
+
+func (c *Client) handleIncoming(messageType int, message []byte) {
+	log.Printf("Received message from user %s: %s", c.UserID, string(message))
+	// Here you can add logic to handle the incoming message, e.g., broadcasting it to other clients
+	c.Hub.Broadcast <- message	
+}
+
+func (c *Client) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.Send:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.Conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			n := len(c.Send)
+			for i := 0; i < n; i++ {
+				w.Write([]byte{'\n'})
+				w.Write(<-c.Send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
